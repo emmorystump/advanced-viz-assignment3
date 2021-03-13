58b0a992e395020596958cd04a7a8e0f2ea18315
Emmory Stump <emmorystump@wustl.edu>
2021-03-13 12:26:26 -0600

    update

--- wrangle_data.py
+++ wrangle_data.py
@@ -7,7 +7,11 @@ import statistics
 from scipy import stats
 import statsmodels.api as sm
 import json 
+import random
 
+def np_encoder(object):
+    if isinstance(object, np.int64): return int(object)  
+    raise TypeError
 
 def isSick(text, identifiers):
     # text = text.lower()
@@ -15,7 +19,7 @@ def isSick(text, identifiers):
     text = text.lower()
     for word in identifiers:
         if text.find(word) != -1:
-            print(text)
+            # print(text)
             return True
     return False
 
@@ -23,7 +27,7 @@ microblogs_data = pd.read_pickle('cse_557_option1/cse557_option1_microblogs.pkl'
 microblogs_data_json = microblogs_data.to_json(orient='records')
 temp = pd.read_json(microblogs_data_json)
 
-print(len(temp.location))
+# print(len(temp.location))
 num_records = len(temp.location)
 
 identifiers = ["flu", "fatigue", "fever", "doctor", "patient", "sick", "sore", "medicine", "pain", "ache", "hurt", "diarrhea", "hospital", "chill", "cough"]
@@ -42,13 +46,28 @@ for i in range(num_records):
     if isSick(text, identifiers):
         sick_posts.append(post_ids[i])
 
-print(len(sick_posts))
+# print(len(sick_posts))
 
-        
 temp.to_json('cse557_option1_microblogs.json')
 
-json_string = json.dumps(sick_posts)
-json_string.to_json('cse557_option1_sick_microblogs.json')
+json_string = json.dumps(sick_posts, default=np_encoder)
+
+with open('cse557_option1_sick_microblogs.json', 'w') as f:
+    json.dump(json_string, f)
+
+# print(json_string)
+# json_string.to_json('cse557_option1_sick_microblogs.json')
+
+
+sampled_sick_posts = random.sample(sick_posts, 1000)
+json_string2 = json.dumps(sampled_sick_posts, default=np_encoder)
+
+with open('cse557_option1_sick_microblogs_sampled.json', 'w') as f:
+    json.dump(json_string2, f)
+
+
+# json_string2.to_json('cse557_option1_sick_microblogs_sampled.json')
+
 
 
 # need to try and filter this so that it only has tweets from sick people
